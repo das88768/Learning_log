f9f318a461ce96a98dbbe355d1173f716beebf76
akash <das88768@gmail.com>
2021-09-08 14:41:42 +0530

    Show the 404 error when topic element does not exist

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from .models import Topic, Entry
@@ -19,7 +19,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """ Shows the topic id and its entries."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     # Make sure that the current topic belogs to the current user.
     if topic.owner != request.user:
         raise Http404
